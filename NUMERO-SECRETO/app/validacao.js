a3ce29ec68b25a688ddab0db70fc6d72c3c589e8
luis-mendes018 <81094313+luis-mendes018@users.noreply.github.com>
2024-08-19 15:39:14 -0300

    Add files via upload

---
+++ NUMERO-SECRETO/app/validacao.js
@@ -0,0 +1,51 @@
+function verificaChute(){
+    const numero = +chute
+
+    if(chuteForInvalido(numero)){
+        //alert('Valor inválido!')
+        elementoChute.innerHTML += '<div>Valor inválido</div>'
+        return
+    }
+
+    if(numeroMaiorOuMenor(numero)){
+
+        //alert(`valor inválido: O número secreto precisa estar entre ${menorValor} e ${maiorValor} `)
+
+        elementoChute.innerHTML += `<div>valor inválido: 
+        Fale um número secreto entre ${menorValor} e ${maiorValor}</div>`
+
+        return
+    }
+
+    if(numero === numeroSecreto){
+        document.body.innerHTML = `
+            <h2>Você acertou!</h2>
+            <h3>O número secreto era ${numeroSecreto}</h3>
+
+            <button id="jogar-novamente" class="btn-jogar">
+                Jogar novamente
+            </button>
+        `
+    } else if(numero > numeroSecreto){
+        elementoChute.innerHTML += `
+        <div>O número secreto é menor <i class="fa-solid fa-arrow-down"></i></div>`
+    }else{
+        elementoChute.innerHTML += `
+            <div>O número secreto é maior <i class="fa-solid fa-arrow-up"></i></div>
+        `
+    }
+}
+
+function chuteForInvalido(numero) {
+    return Number.isNaN(numero)
+}
+
+function numeroMaiorOuMenor(numero){
+    return numero > maiorValor || numero < menorValor
+}
+
+document.body.addEventListener('click', e => {
+    if(e.target.id == 'jogar-novamente'){
+        window.location.reload()
+    }
+})
